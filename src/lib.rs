7d7886f1b822ae841b5728e216575dd35ac60ee5
azraji <zdine30@gmail.com>
2026-02-09 19:02:06 +0100

    done

--- src/lib.rs
+++ src/lib.rs
@@ -1,25 +1,46 @@
 pub mod commend;
 
 pub fn parse(l: &str) {
-    let mut args = Vec::new();
-    let mut current = String::new();
-    let mut in_quotes = false;
-
-    for c in l.chars() {
-        match c {
-            '"' => in_quotes = !in_quotes,
-            ' ' if !in_quotes => {
-                if !current.is_empty() {
-                    args.push(current.clone());
-                    current.clear();
-                }
-            }
-            _ => current.push(c),
+    let arg = l.split("&");
+    let mut le = Vec::new();
+    for s in arg {
+        if s.is_empty() {
+            continue;
         }
-    }
+        let mut args = Vec::new();
+        let mut current = String::new();
+        let mut in_quotes = false;
 
-    if !current.is_empty() {
-        args.push(current);
+        for c in s.chars() {
+            match c {
+                '"' => in_quotes = !in_quotes,
+                ' ' if !in_quotes => {
+                    if !current.is_empty() {
+                        args.push(current.clone());
+                        current.clear();
+                    }
+                }
+                _ => current.push(c),
+            }
+        }
+
+        if !current.is_empty() {
+            args.push(current);
+        }
+        le.push(args);
     }
-    println!("{:?}",args)
+    for i in le {
+        aplye(i)
+    }
+}
+fn aplye(arg: Vec<String>) {
+    if arg.len() == 0 {
+        return;
+    }
+    match arg[0].as_str() {
+        "ls" => {}
+        _ => {
+            println!("commend not found {}",arg[0])
+        },
+    }
 }
