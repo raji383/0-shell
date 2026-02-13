80b7131c44ac99534c3a544ddf0ae413c3da1507
abdrahmanbalouri <balouriabdrahman@gmail.com>
2026-02-13 11:22:30 +0100

    handle pwd

--- src/commend/pwd.rs
+++ src/commend/pwd.rs
@@ -1,12 +1,25 @@
+use crate::escape_special_chars;
 use crossterm::{cursor, execute};
 use std::env;
 use std::io::{self};
 use std::process;
+
 pub fn pwd() {
     match env::current_dir() {
         Ok(path) => {
             execute!(io::stdout(), cursor::MoveToColumn(0),).unwrap();
-            println!("{}", path.display());
+            //  let std = escape_special_chars(&path.to_string_lossy());
+            let k = path.to_string_lossy();
+            let c: Vec<&str> = k.split_whitespace().collect();
+            for (i, v) in c.iter().enumerate() {
+                execute!(io::stdout(), cursor::MoveToColumn(0),).unwrap();
+                println!("{}", v.trim_matches('"'));
+                if c.len() > 1 {
+                    if i == c.len() - 1 {
+                        println!()
+                    }
+                }
+            }
         }
         Err(e) => {
             eprintln!("Error getting current directory: {}", e);
